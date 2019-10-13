d1fe73d287e38f9d82c820f01ad178f697c7a3b1
GreStas <grestas2000@gmail.com>
2019-10-13 12:05:53 +0300

    Пример скрипта для ДЗ-3 Задача-1

---
+++ hw_3_1.py
@@ -0,0 +1,13 @@
+def calc(a):
+    # реализация функции
+    # result = ...
+    result = 100500
+    return result
+
+
+if __name__ == '__main__':
+    a = input('Введите целое число:')
+    # ...
+    result = calc(a)
+    print('В числе', a, 'всего', result, 'цифр.')
+
